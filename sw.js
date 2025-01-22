8ccb7f92b4e7654c1a03c40ac858f09d79fc543f
mrparsing <fralindi5@gmail.com>
2025-01-22 15:25:28 +0100

    update

--- sw.js
+++ sw.js
@@ -1,42 +1,42 @@
-const CACHE_NAME = 'pwa-cache-v1';
+const CACHE_NAME = 'soundtrackmoods-cache-v1';
 const urlsToCache = [
-  '/index.html',
-  '/manifest.json',
-  '/css/index_style.css',
-  '/assets/workout.jpg',
+  'index.html',
+  'manifest.json',
+  'css/index_style.css',
+  'assets/workout.jpg',
 ];
 
-// Installazione del Service Worker
-self.addEventListener('install', function (event) {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(urlsToCache);
-    })
+      caches.open(CACHE_NAME)
+          .then(cache => {
+              return cache.addAll(urlsToCache);
+          })
   );
 });
 
-// Attivazione e pulizia delle vecchie cache
-self.addEventListener('activate', (event) => {
+// Attivazione del Service Worker
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('[Service Worker] Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      caches.keys().then(cacheNames => {
+          return Promise.all(
+              cacheNames.map(cacheName => {
+                  if (cacheName !== CACHE_NAME) {
+                      return caches.delete(cacheName);
+                  }
+              })
+          );
+      })
   );
-  self.clients.claim();
 });
 
-// Recupera i file dalla cache o dalla rete
-self.addEventListener('fetch', (event) => {
+// Recupero delle risorse
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+      caches.match(event.request)
+          .then(response => {
+              // Se c'è una corrispondenza nella cache, restituiscila; altrimenti, fetch from network
+              return response || fetch(event.request);
+          })
   );
 });
